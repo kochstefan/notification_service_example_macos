390b1eb4c54048fb52b8721bcfc0f1844a7cd348
Stefan Koch <stefan.koch10@gmail.com>
2023-04-04 19:45:37 +0200

    Request notification authorization and register for remote notifications

--- app/AppDelegate.m
+++ app/AppDelegate.m
@@ -1,3 +1,4 @@
+#import <UserNotifications/UserNotifications.h>
 #import "AppDelegate.h"
 
 @interface AppDelegate ()
@@ -8,6 +9,12 @@
 @implementation AppDelegate
 
 - (void)applicationDidFinishLaunching:(NSNotification *)aNotification {
+    NSLog(@"applicationDidFinishLaunching");
+    UNUserNotificationCenter *center = [UNUserNotificationCenter currentNotificationCenter];
+    [center requestAuthorizationWithOptions:(UNAuthorizationOptionAlert + UNAuthorizationOptionSound) completionHandler:^(BOOL granted, NSError * _Nullable error) {
+        NSLog(@"requestAuthorizationWithOptionsCompletionHandler%@", error);
+    }];
+    [[NSApplication sharedApplication] registerForRemoteNotifications];
 }
 
 
@@ -17,6 +24,11 @@
 
 - (BOOL)applicationSupportsSecureRestorableState:(NSApplication *)app {
     return YES;
+}
+
+- (void)application:(NSApplication *)application didRegisterForRemoteNotificationsWithDeviceToken:(NSData *)deviceToken
+{
+    NSLog(@"didRegisterForRemoteNotificationsWithDeviceToken");
 }
 
 
